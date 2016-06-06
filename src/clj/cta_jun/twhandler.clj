c3b45fb57b4754cdc80de6d96eedc0c0f4a5d568
Alessio Peternelli <alessio.peternelli@gmail.com>
2016-06-06 22:22:04 +0100

    added doc text to twitter functions, and renamed in a more meaningfull way, kindaof

--- src/clj/cta_jun/twhandler.clj
+++ src/clj/cta_jun/twhandler.clj
@@ -4,17 +4,20 @@
    [twitter.api.restful]))
 
 
-#_(def my-creds (make-oauth-creds "*app-consumer-key*"
+(def my-creds (make-oauth-creds "*app-consumer-key*"
                                 "*app-consumer-secret*"
                                 "*user-access-token*"
                                 "*user-access-token-secret*"))
 
-(defn get-hashtags [tweets]
+(defn normalize-hashtags [tweet-hashtags]
+  "hashes comes as a vector of sets, this function is a shorthand to extract a list with only the hashtags values"
   (map (fn [e] (:text e))
-       tweets))
+       tweet-hashtags))
 
-(defn get-hashtags-raw [tweets-timeline]
+(defn extract-hashtags [tweets-timeline]
+  "as imputs this function take the body of the twitter api https://dev.twitter.com/rest/reference/get/statuses/user_timeline, and return a merged vector of all the hashtags of all the tweets"
   (vec (apply concat (map (fn [t] (:hashtags (:entities t))) tweets-timeline))))
 
 (defn get-timeline [user-screen-name last-n-tweets]
+  "shorhand function to extract the content of the twitter api rest resource statuses/user_timeline"
   (:body (statuses-user-timeline :oauth-creds my-creds :params {:screen-name user-screen-name :count last-n-tweets})))
