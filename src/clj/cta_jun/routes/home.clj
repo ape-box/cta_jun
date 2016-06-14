8bb21d9f803c6b559afeea8f02c868102ed583d9
Alessio Peternelli <alessio.peternelli@gmail.com>
2016-06-14 15:43:01 +0100

    cleaning

--- src/clj/cta_jun/routes/home.clj
+++ src/clj/cta_jun/routes/home.clj
@@ -8,9 +8,6 @@
 (defn home-page []
   (layout/render "home.html"))
 
-(defn about-page []
-  (layout/render "about.html"))
-
 (defn user-page [request]
   (let [screen-name (get (:query-params request) "screen-name") ud (tw/show-user screen-name)]
     (layout/render "user.html" {:screen-name screen-name
@@ -18,13 +15,12 @@
                                 :user-data (map (fn [k] (str (name k) ": " (k ud))) (keys ud))})))
 
 (defn user-hastags [request]
-  (let [screen-name (get (:query-params request) "screen-name") hashtags (tw/show-user-hashtags screen-name)]
+  (let [screen-name (get (:query-params request) "screen-name") hashtags (frequencies (tw/show-user-hashtags screen-name))]
     (layout/render "hashtags.html" {:screen-name screen-name
                                     :hashtags hashtags})))
 
 (defroutes home-routes
   (GET "/" [] (home-page))
-  (GET "/about" [] (about-page))
   (GET "/user" request (user-page request))
   (GET "/hashtags" request (user-hastags request)))
 
--- src/clj/cta_jun/twhandler.clj
+++ src/clj/cta_jun/twhandler.clj
@@ -3,11 +3,10 @@
    [twitter.oauth]
    [twitter.api.restful]))
 
-
-(def my-creds (make-oauth-creds "*app-consumer-key*"
-                                "*app-consumer-secret*"
-                                "*user-access-token*"
-                                "*user-access-token-secret*"))
+(def my-creds (make-oauth-creds (System/getenv "OAUTH_APP_KEY")
+                                (System/getenv "OAUTH_APP_SECRET")
+                                (System/getenv "OAUTH_CONSUMER_KEY")
+                                (System/getenv "OAUTH_CONSUMER_SECRET")))
 
 (defn normalize-hashtags [tweet-hashtags]
   "hashes comes as a vector of sets, this function is a shorthand to extract a list with only the hashtags values"
@@ -15,15 +14,17 @@
        tweet-hashtags))
 
 (defn extract-hashtags [tweets-timeline]
-  "as imputs this function take the body of the twitter api https://dev.twitter.com/rest/reference/get/statuses/user_timeline, and return a merged vector of all the hashtags of all the tweets"
+  "as inputs this function take the body of the twitter api https://dev.twitter.com/rest/reference/get/statuses/user_timeline, and return a merged vector of all the hashtags of all the tweets"
   (vec (apply concat (map (fn [t] (:hashtags (:entities t))) tweets-timeline))))
 
 (defn get-timeline [user-screen-name last-n-tweets]
-  "shorhand function to extract the content of the twitter api rest resource statuses/user_timeline"
+  "shorthand function to extract the content of the twitter api rest resource statuses/user_timeline"
   (:body (statuses-user-timeline :oauth-creds my-creds :params {:screen-name user-screen-name :count last-n-tweets})))
 
 (defn show-user [user-screen-name]
+  "make a call to the twitter api and return only the data"
   (:body (users-show :oauth-creds my-creds :params {:screen-name user-screen-name})))
 
 (defn show-user-hashtags [user-screen-name]
+  "make a call to the twitter api and return the last 500 hashes, it doesn't count them or remove the duplicates"
   (normalize-hashtags (extract-hashtags (get-timeline user-screen-name 500))))
